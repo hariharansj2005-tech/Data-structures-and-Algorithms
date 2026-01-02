38e7237087e4b8823225a5272e92c8c1600e9315
hariharansj2005-tech <hariharansj2005@gmail.com>
2026-01-02 09:45:16 +0530

    MinHeap.c
    Implement a Min Heap data structure with insertion and display functionality.

---
+++ Tree/MinHeap.c
@@ -0,0 +1,33 @@
+//Min Heap.c
+
+int main()
+{
+    int heap[10];
+    int value,i,n,size=0;
+    printf("Enter number of elements : ");
+    scanf("%d",&n);
+    
+    printf("Enter array values : ");
+    for(int j=0;j<n;j++)
+    {
+        scanf("%d",&value);
+        heap[size]=value;
+        i=size;
+        size++;
+        
+        while(i>0 && heap[(i-1)/2]>heap[i])
+        {
+            int temp=heap[i];
+            heap[i]=heap[(i-1)/2];
+            heap[(i-1)/2]=temp;
+            i=(i-1)/2;
+        }
+    }
+    printf("Min Heap : ");
+    for(int k=0;k<size;k++)
+    {
+        printf("%d ",heap[k]);
+    }
+}
+
+      
